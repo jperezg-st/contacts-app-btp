4a5c8fb650bf5fec022ba812d18d63d3b2a0fd52
jperezg-st <lnaokzl@gmail.com>
2021-05-10 12:39:46 -0500

    models change

--- contacts/models.py
+++ contacts/models.py
@@ -3,11 +3,11 @@ from django.core.validators import RegexValidator
 # Create your models here.
 alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
 class Contact(models.Model):
-    name = models.CharField(max_length=250, blank=True, null=True)
-    last_name= models.CharField(max_length=250)
-    company = models.CharField(max_length=1000)
-    email = models.EmailField(max_length=254, unique=True)
-    phone_number = models.CharField(max_length=20)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    last_name= models.CharField(max_length=50)
+    company = models.CharField(max_length=20)
+    email = models.EmailField(max_length=54, unique=True)
+    phone_number = models.CharField(max_length=15)
 
     def __str__(self):
           return self.name
