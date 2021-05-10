01096472deea4042ea81d94cd509496b782f5ad0
jperezg-st <lnaokzl@gmail.com>
2021-05-10 09:01:18 -0500

    unexpected indent

--- contactslist/settings.py
+++ contactslist/settings.py
@@ -81,13 +81,13 @@ WSGI_APPLICATION = 'contactslist.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-  import dj_database_url
+import dj_database_url
 
-  DATABASES = {
-      'default': dj_database_url.config(
-          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-      )
-  }
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
+}
 
 
 # Password validation
